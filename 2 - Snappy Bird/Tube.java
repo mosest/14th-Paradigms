b75525c41f6ad896fadc51eff1d3b0f09d79dd63
Tara <colorsplusplus@gmail.com>
2016-01-24 23:55:02 -0600

    pipes now have random height and up/down facing. no collision detection yet tho hehhe

--- 2 - Snappy Bird/Tube.java
+++ 2 - Snappy Bird/Tube.java
@@ -17,17 +17,33 @@ class Tube {
 		this.tube_x = x;
 		this.tube_y = y;
 		this.facing_up = true;
-		
-		if (facing_up) this.tube_image = ImageIO.read(new File("tubeup.png"));
-		else this.tube_image = ImageIO.read(new File("tubedown.png"));
+
+		this.tube_image = ImageIO.read(new File("tubeup.png"));
 	}
 	
-	public void update() {
+	public void update(int random1, int random2) throws IOException {
 		// Move tube a little to the left
 		tube_x -= 5;
 		
 		// If tube goes off-screen to the left,
 		// put it a little off-screen to the right
-		if (tube_x < -44) tube_x = 501;
+		if (tube_x < -44) { // tube sprite is ~44 pixels wide
+			tube_x = 501; 
+			
+			// Randomly decide whether the tube faces up or down
+			if (random2 == 0) facing_up = true;
+			else facing_up = false;
+			
+			// Reset the tube sprite if you need to
+			if (facing_up) this.tube_image = ImageIO.read(new File("tubeup.png"));
+			else this.tube_image = ImageIO.read(new File("tubedown.png"));
+			
+			// Create random height of tube
+			tube_y = random1;
+			
+			System.out.println("random1 = " + random1 + ", height = " + tube_y);
+		}
+		
+		
 	}
 }
